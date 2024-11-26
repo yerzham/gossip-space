04d75387a0221ae3b9e0f1dc09894462a79f686d
Yerzhan Zhamashev <yerzhan.zhamashev@aalto.fi>
2024-11-26 17:59:31 +0200

    format fix

--- app/components/camera-controls.tsx
+++ app/components/camera-controls.tsx
@@ -4,14 +4,20 @@ import { useFrame } from "@react-three/fiber";
 import { useCallback } from "react";
 
 const CameraControls = ({ maxDistance }: { maxDistance: number }) => {
-  const zoom = useCallback(function zoom(this: {camera: THREE.Camera}, event: WheelEvent) {
+  const zoom = useCallback(function zoom(
+    this: { camera: THREE.Camera },
+    event: WheelEvent,
+  ) {
     const delta = event.deltaY;
     const direction = new THREE.Vector3(0, 0, -1);
     this.camera.position.lerp(
       this.camera.position.clone().add(direction.multiplyScalar(delta)),
-      0.1
+      0.1,
     );
-    this.camera.position.z = Math.min(Math.max(this.camera.position.z, 20), maxDistance);
+    this.camera.position.z = Math.min(
+      Math.max(this.camera.position.z, 20),
+      maxDistance,
+    );
   }, []);
 
   useFrame(({ gl, camera }) => {
@@ -28,12 +34,12 @@ const CameraControls = ({ maxDistance }: { maxDistance: number }) => {
 
     camera.position.x = Math.min(
       Math.max(camera.position.x + direction.x * 0.1, -world.xDim / 2),
-      world.xDim / 2
+      world.xDim / 2,
     );
 
     camera.position.y = Math.min(
       Math.max(camera.position.y + direction.y * 0.1, -world.yDim / 2),
-      world.yDim / 2
+      world.yDim / 2,
     );
   });
 
